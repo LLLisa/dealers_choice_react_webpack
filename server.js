e6ac49beb6062d7149a63d80ce2fdca60eb7ff25
Lisa Knox <lisaknox83@gmail.com>
2022-03-07 10:06:02 -0500

    ready for deployment

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const faker = require('faker');
 const Sequelize = require('sequelize');
 const db = new Sequelize(
   process.env.DATABASE_URL ||
-    'postgres://localhost/dealers_choice_react_webpack',
-  { logging: false } //love this
+    'postgres://localhost/dealers_choice_react_webpack'
+  // { logging: false } //love this
 );
 
 const Human = db.define('human', {
