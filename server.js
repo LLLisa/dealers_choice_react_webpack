fdcd0798bb5e7ddf474febbc0d626752551ceefb
Lisa Knox <lisaknox83@gmail.com>
2022-03-07 13:25:08 -0500

    even more heroku testing

--- server.js
+++ server.js
@@ -3,13 +3,7 @@ const faker = require('faker');
 const Sequelize = require('sequelize');
 const db = new Sequelize(
   process.env.DATABASE_URL ||
-    'postgres://localhost/dealers_choice_react_webpack',
-  {
-    ssl: true,
-    dialectOptions: {
-      ssl: true,
-    },
-  }
+    'postgres://localhost/dealers_choice_react_webpack'
   // { logging: false } //love this
 );
 
